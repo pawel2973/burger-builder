6d7dea6d59d63f3fdce90e59bebe8cdba4490c81
pawel2973 <pawel2973@gmail.com>
2019-04-12 21:07:00 +0200

    Outputting Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,29 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
  * Responsible for render Burger
  */
 const burger = (props) => {
-    return(
+    /** 1. Object.keys(): extract the keys from given Obj. Return array of the keys.
+     *  2. In each iteration, we create a table with as many empty fields as the number of a given ingredient.
+     *  3. For each of the created empty arrays, we return as many components as the length of the given array.
+     *
+     *  1. Object.keys(): wyciąga klucze z danego obiektu. Zwraca tablicę tych kluczy.
+     *  2. W kazdej iteracji tworzymy tablice z taka iloscia pustych pol jak liczba danego skladnika.
+     *  3. Dla kazdej ze stworzonych pustych tablic zwracamy tyle komponentów ile wynosi długość tablicy.
+     */
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])] //ingredients['meat'] = 2 etc. [ , ]
+                .map((_, i) => {
+                    return <BurgerIngredient key={igKey + i} type={igKey}/>
+                });
+        });
+
+    console.log(Object.keys(props.ingredients));
+
+    return (
         <div className={classes.Burger}>
-            <BurgerIngredient type={"bread-top"} />
-            <BurgerIngredient type={"cheese"} />
-            <BurgerIngredient type={"meat"} />
-            <BurgerIngredient type={"bread-bottom"} />
+            <BurgerIngredient type={"bread-top"}/>
+            {transformedIngredients}
+            <BurgerIngredient type={"bread-bottom"}/>
         </div>
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,10 +7,29 @@ import Burger from '../../components/Burger/Burger';
  * Responsible for creating burger
  */
 class BurgerBuilder extends Component {
+    // Alternative way to initialize the state
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        // object with key - value pairs
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 2,
+            meat: 1
+        }
+    };
+
     render() {
+        console.log("salad: " + this.state.ingredients['salad']);
         return (
             <Aux>
-                <Burger />
+                <Burger
+                    ingredients={this.state.ingredients}
+                />
                 <div>Build Controls</div>
             </Aux>
         );
